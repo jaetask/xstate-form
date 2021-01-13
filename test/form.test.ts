4ddb5d541b1671635449f29c29d35bc426aae522
jaetask <me@jaetask.com>
2021-01-13 17:28:30 +0000

    Clean

--- test/form.test.ts
+++ test/form.test.ts
@@ -2,90 +2,14 @@ import { Machine } from 'xstate';
 // import * as Yup from 'yup';
 import { buildMachine } from './machines/form.machine';
 
-// scratch pad - all in one file
-
-// credit to Formik for this function, add to docs
-// this might not match what I want, test and see..
-// function yupToFormErrors(validated: any, touched: any) {
-//   let errors: { [key: string]: any } | null = {};
-//   if (Array.isArray(validated.inner) && validated.inner.length) {
-//     validated.inner.forEach((validationError: any) => {
-//       const field = validationError.path;
-//       if (touched[field]) {
-//         errors![field] = validationError?.errors[0] || '';
-//       } else if (errors![field]) {
-//         delete errors![field];
-//       }
-//     });
-//   } else if (Array.isArray(validated.errors) && validated.errors.length) {
-//     // console.log('validated', validated);
-//     const { message = '', path: field } = validated;
-//     if (touched[field]) {
-//       errors![field] = message;
-//     }
-//   }
-
-//   return errors;
-// }
-
-// const schema = Yup.object({
-//   query: Yup.string()
-//     .min(2)
-//     .required(),
-// });
-
-// const form = (machineConfig: any, machineOptions: any, schema: any) => {
-//   let state = null;
-//   let errors: { [key: string]: string } = {};
-//   const machine = Machine(machineConfig, machineOptions);
-//   const service = interpret(machine);
-
-//   service.onTransition(currentState => {
-//     if (currentState.changed) {
-//       state = currentState;
-//       try {
-//         schema.validateSync(state.context);
-//         errors = {};
-//       } catch (formErrors) {
-//         errors = yupToFormErrors(formErrors, touched);
-//         console.log('fomr->errors', errors);
-//       }
-//     }
-//   });
-
-//   service.start();
-
-//   const touched: { [key: string]: boolean } = {};
-//   const hasError = (fieldPath: string): boolean =>
-//     errors[fieldPath] === undefined ? false : true;
-//   const isTouched = (fieldPath: string): boolean =>
-//     touched[fieldPath] !== undefined;
-//   const isInvalid = (fieldPath: string): boolean =>
-//     hasError(fieldPath) && isTouched(fieldPath);
-//   const error = (fieldPath: string): string | undefined =>
-//     errors[fieldPath] === undefined ? undefined : errors[fieldPath];
-//   const touch = (fieldPath: string): void => {
-//     touched[fieldPath] = true;
-//   };
-
-//   const validator = {
-//     errors,
-//     error,
-//     hasError,
-//     isInvalid,
-//     isTouched,
-//     touch,
-//   };
-
-//   return { state, send: service.send, service, validator };
-// };
-
+// define action creators
 const focus = (name: string) => ({ type: 'FOCUS', fieldName: name });
 const blur = (name: string) => ({ type: 'BLUR', fieldName: name });
 const disable = (name: string) => ({ type: 'DISABLE', fieldName: name });
 const enable = (name: string) => ({ type: 'ENABLE', fieldName: name });
 const change = (name: string, value: string | number | boolean) => ({ type: 'CHANGE', fieldName: name, value });
 
+// test utils
 const transitions = (machine: any, events: any[], initialState: any) =>
   events.reduce((current, event) => machine.transition(current, event), initialState);
 
@@ -163,3 +87,81 @@ describe('xstate-form', () => {
 
   // RESETs happen at the field level, each field can react however it wants.
 });
+
+// scratch pad - all in one file
+
+// credit to Formik for this function, add to docs
+// this might not match what I want, test and see..
+// function yupToFormErrors(validated: any, touched: any) {
+//   let errors: { [key: string]: any } | null = {};
+//   if (Array.isArray(validated.inner) && validated.inner.length) {
+//     validated.inner.forEach((validationError: any) => {
+//       const field = validationError.path;
+//       if (touched[field]) {
+//         errors![field] = validationError?.errors[0] || '';
+//       } else if (errors![field]) {
+//         delete errors![field];
+//       }
+//     });
+//   } else if (Array.isArray(validated.errors) && validated.errors.length) {
+//     // console.log('validated', validated);
+//     const { message = '', path: field } = validated;
+//     if (touched[field]) {
+//       errors![field] = message;
+//     }
+//   }
+
+//   return errors;
+// }
+
+// const schema = Yup.object({
+//   query: Yup.string()
+//     .min(2)
+//     .required(),
+// });
+
+// const form = (machineConfig: any, machineOptions: any, schema: any) => {
+//   let state = null;
+//   let errors: { [key: string]: string } = {};
+//   const machine = Machine(machineConfig, machineOptions);
+//   const service = interpret(machine);
+
+//   service.onTransition(currentState => {
+//     if (currentState.changed) {
+//       state = currentState;
+//       try {
+//         schema.validateSync(state.context);
+//         errors = {};
+//       } catch (formErrors) {
+//         errors = yupToFormErrors(formErrors, touched);
+//         console.log('fomr->errors', errors);
+//       }
+//     }
+//   });
+
+//   service.start();
+
+//   const touched: { [key: string]: boolean } = {};
+//   const hasError = (fieldPath: string): boolean =>
+//     errors[fieldPath] === undefined ? false : true;
+//   const isTouched = (fieldPath: string): boolean =>
+//     touched[fieldPath] !== undefined;
+//   const isInvalid = (fieldPath: string): boolean =>
+//     hasError(fieldPath) && isTouched(fieldPath);
+//   const error = (fieldPath: string): string | undefined =>
+//     errors[fieldPath] === undefined ? undefined : errors[fieldPath];
+//   const touch = (fieldPath: string): void => {
+//     touched[fieldPath] = true;
+//   };
+
+//   const validator = {
+//     errors,
+//     error,
+//     hasError,
+//     isInvalid,
+//     isTouched,
+//     touch,
+//   };
+
+//   return { state, send: service.send, service, validator };
+// };
