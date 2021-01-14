926ed6d55b8c563d812a3b6bb7e03109d7d40548
jaetask <me@jaetask.com>
2021-01-14 16:59:04 +0000

    export items

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+// import all the things to expose
+import * as actions from './actions';
+import * as conditions from './conditions';
+import * as fields from './fields';
+import * as form from './form';
+
+export { actions, conditions, fields, form };
